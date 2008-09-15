396e83aa57dd9c9fe623f08f6d76a077a2bc23de
Jim Gay <jim@saturnflyer.com>
2008-09-15 11:04:48 -0400

    cleaned up code and specs

--- app/models/sibling_tags.rb
+++ app/models/sibling_tags.rb
@@ -2,156 +2,157 @@ module SiblingTags
   include Radiant::Taggable
   
   desc %{
-    Gives access to a page's siblings. 
+    Set's the scope for a page's siblings. 
   }
   tag 'siblings' do |tag|
-    if tag.locals.page.parent
-      tag.locals.siblings = tag.locals.page.parent.children
-      tag.expand
-    end
+    tag.expand
   end
-  
-  desc %{
-    Only renders the contents of this tag if the current page has any published siblings.
-  }
-  tag 'if_siblings' do |tag|
-    if parent = tag.locals.page.parent
-      if parent.children.size > 1
+    
+    desc %{
+      Loops through each sibling and outputs the contents
+    }
+    tag 'siblings:each' do |tag|
+      result = []
+      tag.locals.siblings = tag.locals.page.parent.children.find(:all, siblings_find_options(tag))
+      tag.locals.siblings.each do |sib|
+        tag.locals.page = sib
+        result << tag.expand
+      end
+      result
+    end
+    
+    desc %{
+      Only renders the contents of this tag if the current page has any published siblings.
+    }
+    tag 'if_siblings' do |tag|
+      if parent = tag.locals.page.parent
+        if parent.children.find(:all, siblings_find_options(tag)).size > 0
+          tag.expand
+        end
+      end
+    end
+    
+    desc %{
+      Only renders the contents of this tag if the current page has no published siblings.
+    }
+    tag 'unless_siblings' do |tag|
+      if !tag.locals.page.parent or tag.locals.page.parent.children.find(:all, siblings_find_options(tag)).size == 0
         tag.expand
       end
     end
-  end
-  
-  desc %{
-    Only renders the contents of this tag if the current page has no published siblings.
-  }
-  tag 'unless_siblings' do |tag|
-    if !tag.locals.page.parent or tag.locals.page.parent.children.size <= 1
-      tag.expand
+    
+    desc %{
+      Only render the contents of this tag if the current page has a sibling *after* it, when sorted according to the @order@ and @by@ options. 
+      
+      See @<siblings:next/>@ for a more detailed description of the sorting options.
+    }
+    tag 'siblings:if_next' do |tag|
+      tag.expand if find_next_sibling(tag)
     end
-  end
-  
-  
-  
-  desc %{
-    Only render the contents of this tag if the current page has a sibling *after* it, when sorted according to the @order@ and @by@ options. 
     
-    See @<siblings:next/>@ for a more detailed description of the sorting options.
-  }
-  tag 'siblings:if_next' do |tag|
-    if next_sibling = find_next_sibling(tag)
-      tag.expand
+    desc %{
+      Only render the contents of this tag if the current page has a sibling *before* it, when sorted according to the @order@ and @by@ options. 
+      
+      See @<siblings:next/>@ for a more detailed description of the sorting options.
+    }
+    tag 'siblings:if_previous' do |tag|
+      tag.expand if find_previous_sibling(tag)
     end
-  end
-  
-  desc %{
-    Only render the contents of this tag if the current page has a sibling *before* it, when sorted according to the @order@ and @by@ options. 
     
-    See @<siblings:next/>@ for a more detailed description of the sorting options.
-  }
-  tag 'siblings:if_previous' do |tag|
-    if previous_sibling = find_previous_sibling(tag)
-      tag.expand
+    desc %{
+      Only render the contents of this tag if the current page is the last of its siblings, when sorted according to the @order@ and @by@ options. 
+      
+      See @<siblings:next/>@ for a more detailed description of the sorting options.
+    }
+    tag 'siblings:unless_next' do |tag|
+      tag.expand unless find_next_sibling(tag)
     end
-  end
-  
-  desc %{
-    Only render the contents of this tag if the current page is the last of its siblings, when sorted according to the @order@ and @by@ options. 
     
-    See @<siblings:next/>@ for a more detailed description of the sorting options.
-  }
-  tag 'siblings:unless_next' do |tag|
-    unless next_sibling = find_next_sibling(tag)
-      tag.expand
+    desc %{
+      Only render the contents of this tag if the current page is the first of its siblings, when sorted according to the @order@ and @by@ options. 
+      
+      See @<siblings:next/>@ for a more detailed description of the sorting options.
+    }
+    tag 'siblings:unless_previous' do |tag|
+      tag.expand unless find_previous_sibling(tag)
     end
-  end
-
-  desc %{
-    Only render the contents of this tag if the current page is the first of its siblings, when sorted according to the @order@ and @by@ options. 
     
-    See @<siblings:next/>@ for a more detailed description of the sorting options.
-  }
-  tag 'siblings:unless_previous' do |tag|
-    unless previous_sibling = find_previous_sibling(tag)
-      tag.expand
-    end
-  end
-
-  desc %{
-    All Radiant tags within a @<r:siblings:next/>@ block are interpreted in the context
-    of the next sibling page. 
-    
-    The order in which siblings are sorted can be manipulated using all the same attributes
-    as the @<r:children:each/>@ tag. If no attributes are supplied, the siblings will
-    have order = "published_at ASC". The @by@ attribute allows you to order by any page 
-    properties stored in the database, the most likely of these to be useful are @published_at@
-    and @title@.
-    
-    *Usage:*
-    <pre><code><r:siblings:next [by="published_at|title"] [order="asc|desc"] [status="published|all"]/>...</r:siblings:next></code></pre>
-  }
-  tag 'siblings:next' do |tag|
-    if next_sibling = find_next_sibling(tag)
-      tag.locals.page = next_sibling
-      tag.expand
-    end
-  end
-  
-  desc %{
-    All Radiant tags within a @<r:siblings:previous/>@ block are interpreted in the context
-    of the previous sibling page, when sorted according to the @order@ and @by@ options.
-    
-    See @<siblings:next/>@ for a more detailed description of the sorting options.
-    
-    *Usage:*
-    <pre><code><r:siblings:previous [by="published_at|title"] [order="asc|desc"] 
-    [status="published|all"]/>...</r:siblings:previous></code></pre>
-  }
-  tag 'siblings:previous' do |tag|
-    if previous_sibling = find_previous_sibling(tag)
-      tag.locals.page = previous_sibling
-      tag.expand
-    end
-  end
-  
-  private
-  
-  def find_next_sibling(tag)
-    tag.locals.siblings.find(:first, siblings_find_options(tag,"next"))
-  end
-  
-  def find_previous_sibling(tag)
-    sorted = tag.locals.siblings.find(:all, siblings_find_options(tag,"previous"))
-    sorted.last unless sorted.empty?
-  end
-  
-  def siblings_find_options(tag,direction="next")
-    # from Radiant core, children_find_options returns something like:
-    # { :conditions => ["(virtual = ?) and (status_id = ?)", false, 100], 
-    #   :order=>"published_at ASC" }
-    options = children_find_options(tag)
-    attr = tag.attr.symbolize_keys
-
-    by = (attr[:by] || 'published_at').strip
-    order = (attr[:order] || 'asc').strip
-    
-    if order.downcase == "asc"
-      if direction == "previous"
-        next_condition =  " and (#{by} < ?)"
-      else
-        next_condition = " and (#{by} > ?)"
-      end
-    else
-      if direction == "previous"
-        next_condition = " and (#{by} > ?)"
-      else
-        next_condition = " and (#{by} < ?)"
+    desc %{
+      All Radiant tags within a @<r:siblings:next/>@ block are interpreted in the context
+      of the next sibling page. 
+      
+      The order in which siblings are sorted can be manipulated using all the same attributes
+      as the @<r:children:each/>@ tag. If no attributes are supplied, the siblings will
+      have order = "published_at ASC". The @by@ attribute allows you to order by any page 
+      properties stored in the database, the most likely of these to be useful are @published_at@
+      and @title@.
+      
+      *Usage:*
+      <pre><code><r:siblings:next [by="published_at|title"] [order="asc|desc"] [status="published|all"]/>...</r:siblings:next></code></pre>
+    }
+    tag 'siblings:next' do |tag|
+      if tag.locals.page = find_next_sibling(tag)
+        tag.expand
       end
     end
     
-    options[:conditions].first << next_condition
-    options[:conditions] << tag.locals.page.send(by)
-    options
-  end
-  
+    desc %{
+      All Radiant tags within a @<r:siblings:previous/>@ block are interpreted in the context
+      of the previous sibling page, when sorted according to the @order@ and @by@ options.
+      
+      See @<siblings:next/>@ for a more detailed description of the sorting options.
+      
+      *Usage:*
+      <pre><code><r:siblings:previous [by="published_at|title"] [order="asc|desc"] 
+      [status="published|all"]/>...</r:siblings:previous></code></pre>
+    }
+    tag 'siblings:previous' do |tag|
+      if tag.locals.page = find_previous_sibling(tag)
+        tag.expand
+      end
+    end
+    
+    private
+    
+    def find_next_sibling(tag)
+      if tag.locals.page.parent
+        tag.attr['adjacent'] = 'next'
+        tag.locals.page.parent.children.find(:first, adjacent_siblings_find_options(tag))
+      end
+    end
+    
+    def find_previous_sibling(tag)
+      if tag.locals.page.parent
+        tag.attr['adjacent'] = 'previous'
+        tag.locals.page.parent.children.find(:first, adjacent_siblings_find_options(tag))
+      end
+    end
+    
+    def adjacent_siblings_find_options(tag)
+      options = siblings_find_options(tag)
+      adjacent_condition = attr_or_error(tag, :attribute_name => 'adjacent', :default => 'next', :values => 'next, previous')
+      attribute_sort = (tag.attr[:by] || 'published_at').strip
+      attribute_order = attr_or_error(tag, :attribute_name => 'order', :default => 'asc', :values => 'desc, asc')
+      
+      find_less_than    = " and (#{attribute_sort} < ?)"
+      find_greater_than = " and (#{attribute_sort} > ?)"
+      
+      if attribute_order == "asc"
+        adjacent_find_condition = (adjacent_condition == 'previous' ? find_less_than : find_greater_than)
+      else
+        adjacent_find_condition = (adjacent_condition == 'previous' ? find_greater_than : find_less_than)
+      end
+      
+      options[:conditions].first << adjacent_find_condition
+      options[:conditions] << tag.locals.page.send(attribute_sort)
+      
+      options
+    end
+    
+    def siblings_find_options(tag)
+      options = children_find_options(tag)
+      options[:conditions].first << ' and id != ?'
+      options[:conditions] << tag.locals.page.id
+      options
+    end
 end
--- spec/models/sibling_tags_spec.rb
+++ spec/models/sibling_tags_spec.rb
@@ -3,118 +3,107 @@ require File.dirname(__FILE__) + '/../spec_helper'
 describe "Sibling Tags" do
   scenario :sibling_pages
   
-  describe "called from the home page" do
-    it "should output nothing" do
-      page(:home).should render('<r:siblings:next><r:title/></r:siblings:next>').as('')
+  describe "<r:siblings>" do
+    it "should expand its contents" do
+      page(:sneezy).should render('<r:siblings>true</r:siblings>').as('true')
     end
-    it "should not have any siblings" do
-      page(:home).should render('<r:if_siblings>NO!</r:if_siblings>').as('')
-      page(:home).should render('<r:unless_siblings>YES</r:unless_siblings>').as('YES')
+  end
+  describe "<r:siblings:each>" do
+    it "should order the page siblings by published_at" do
+      page(:sneezy).should render('<r:siblings:each><r:title/> </r:siblings:each>').as('Happy Grumpy Dopey Doc Bashful ')
+    end
+    it "should allow siblings to be ordered by the 'by' attribute" do
+      page(:sneezy).should render('<r:siblings:each by="title"><r:title /> </r:siblings:each>').as('Bashful Doc Dopey Grumpy Happy ')
+    end
+    it "should allow siblings to be sorted with the 'order' attribute when using 'by'" do
+      page(:sneezy).should render('<r:siblings:each by="slug" order="asc"><r:title /> </r:siblings:each>').as('Bashful Doc Dopey Grumpy Happy ')
+    end
+    it "should exclude the current page" do
+      page(:sneezy).should render('<r:siblings:each><r:title/> </r:siblings:each>').as('Happy Grumpy Dopey Doc Bashful ')
+    end
+    it "should exclude unpublished pages" do
+      page(:sneezy).should render('<r:siblings:each><r:title/> </r:siblings:each>').as('Happy Grumpy Dopey Doc Bashful ')
+    end
+  end  
+      
+  describe "<r:if_siblings>" do
+    it 'should output its contents if the current page has siblings' do
+      page(:happy).should render('<r:if_siblings>true</r:if_siblings>').as('true')
+    end
+    it 'should not output its contents if the current page has no siblings' do
+      page(:solo).should render('<r:if_siblings>false</r:if_siblings>').as('')
     end
   end
   
-  describe "called from an only child" do
-    
-    it "should not have any siblings" do
-      page(:solo).should render('<r:if_siblings>NO!</r:if_siblings>').as('')
-      page(:solo).should render('<r:unless_siblings>YES</r:unless_siblings>').as('YES')
+  describe "<r:unless_siblings>" do
+    it 'should output its contents if the current page has no siblings' do
+      page(:solo).should render('<r:unless_siblings>true</r:unless_siblings>').as('true')
     end
-    
-    it "should render contents of <r:siblings/> when there are none" do
-      page(:solo)
-      page.should render('<r:siblings>visible</r:siblings>').as('visible')
-    end
-    
-    it "should not render unless_next/previous contents" do
-      page(:solo)
-      page.should render('<r:siblings><r:if_next>invisible</r:if_next></r:siblings>').as('')
-      page.should render('<r:siblings><r:if_previous>invisible</r:if_previous></r:siblings>').as('')
-    end
-    
-    it "should render unless_next/previous contents" do
-      page(:solo)
-      page.should render('<r:siblings><r:unless_next>visible</r:unless_next></r:siblings>').as('visible')
-      page.should render('<r:siblings><r:unless_previous>visible</r:unless_previous></r:siblings>').as('visible')
-    end
-    
-    it "should render nothing for <r:next/> tag" do
-      page(:solo)
-      page.should render('<r:siblings:next><r:title/></r:siblings:next>').as('')
-    end
-    
-    it "should render nothing for <r:previous/> tag" do
-      page(:solo)
-      page.should render('<r:siblings:previous><r:title/></r:siblings:previous>').as('')
-    end
-    
-  end
-  
-  describe "called from a child with a sibling" do
-    
-    it "should have a sibling" do
-      page(:kate).should render('<r:if_siblings>YES</r:if_siblings>').as('YES')
-      page(:kate).should render('<r:unless_siblings>NO!</r:unless_siblings>').as('')
-    end
-    
-    it "should use next page with default sort options" do
-      page(:kate)
-      page.should render('<r:siblings:next><r:title/></r:siblings:next>').as('Amy')
-    end
-    it "should use previous page with default sort options" do
-      page(:amy)
-      page.should render('<r:siblings:previous><r:title/></r:siblings:previous>').as('Kate')
-    end
-    
-    it "should use next page when sort order='desc'" do
-      page(:amy)
-      page.should render('<r:siblings:next order="desc"><r:title/></r:siblings:next>').as('Kate')
-    end
-    it "should use previous page when sort order='desc'" do
-      page(:kate)
-      page.should render('<r:siblings:previous order="desc"><r:title/></r:siblings:previous>').as('Amy')
-    end
-    
-    it "should use next page when sort by='title'" do
-      page(:amy)
-      page.should render('<r:siblings:next by="title"><r:title/></r:siblings:next>').as('Kate')
-    end
-    it "should use previous page when sort by='title'" do
-      page(:kate)
-      page.should render('<r:siblings:previous by="title"><r:title/></r:siblings:previous>').as('Amy')
-    end
-    
-    it "should use next page when sort by='title' and order='desc'" do
-      page(:kate)
-      page.should render('<r:siblings:next by="title" order="desc"><r:title/></r:siblings:next>').as('Amy')
-    end
-    it "should use previous page when sort by='title' and order='desc'" do
-      page(:amy)
-      page.should render('<r:siblings:previous by="title" order="desc"><r:title/></r:siblings:previous>').as('Kate')
+    it 'should not output its contents if the current page has siblings' do
+      page(:happy).should render('<r:unless_siblings>false</r:unless_siblings>').as('')
     end
   end
   
-  describe "called from a child with many siblings" do
-    it "should skip unpublished pages when looking for next" do
-      page(:happy).should render('<r:siblings:next by="title"><r:title/></r:siblings:next>').as('Sneezy')
-      page(:sneezy).should render('<r:siblings:next><r:title/></r:siblings:next>').as('Happy')
+  describe "<r:siblings:next>" do
+    it 'should output nothing if the current page has no siblings' do
+      page(:home).should render('<r:siblings:next>false</r:siblings:next>').as('')
     end
-    
-    it "should skip unpublished pages when looking for previous" do
-      page(:sneezy).should render('<r:siblings:previous by="title"><r:title/></r:siblings:previous>').as('Happy')
-      page(:happy).should render('<r:siblings:previous><r:title/></r:siblings:previous>').as('Sneezy')
+    it 'should output its contents if the current page has a sibling next in order' do
+      page(:doc).should render('<r:siblings:next>true</r:siblings:next>').as('true')
     end
-  end
-  
-  describe "called from scope other than the current page" do
-    it "should operate within the specified scope" do
-      page(:home)
-      page.should render('<r:find url="/mother-of-two"><r:children:first><r:siblings:next><r:title/></r:siblings:next></r:children:first></r:find>').as('Amy')
-      page.should render('<r:find url="/mother-of-two"><r:children:last><r:siblings:previous><r:title/></r:siblings:previous></r:children:last></r:find>').as('Kate')
-      page.should render('<r:find url="/mother-of-two"><r:children:last><r:siblings:next><r:title/></r:siblings:next></r:children:last></r:find>').as('')
-      page.should render('<r:find url="/mother-of-two"><r:children:first><r:siblings:previous><r:title/></r:siblings:previous></r:children:first></r:find>').as('')
+    it 'should not output its contents if the current page has siblings, but not next in order' do
+      page(:bashful).should render('<r:siblings:next>true</r:siblings:next>').as('')
     end
   end
   
+  describe "<r:siblings:previous>" do
+    it 'should output nothing if the current page has no siblings' do
+      page(:home).should render('<r:siblings:previous>false</r:siblings:previous>').as('')
+    end
+    it 'should output its contents if the current page has a sibling previous in order' do
+      page(:doc).should render('<r:siblings:previous>true</r:siblings:previous>').as('true')
+    end
+    it 'should not output its contents if the current page has siblings, but not previous in order' do
+      page(:sneezy).should render('<r:siblings:previous>true</r:siblings:previous>').as('')
+    end
+  end
+  
+  describe "<r:sibling:if_next>" do
+    it "should output its contents if the current page has a sibling next in order" do
+      page(:doc).should render('<r:siblings:if_next>true</r:siblings:if_next>').as('true')
+    end
+    it "should not output its contents if the current page has no sibling next in order" do
+      page(:bashful).should render('<r:siblings:if_next>true</r:siblings:if_next>').as('')
+    end
+  end
+  
+  describe "<r:siblings:unless_next>" do
+    it "should output its contents if the current page has no sibling next in order" do
+      page(:bashful).should render('<r:siblings:unless_next>true</r:siblings:unless_next>').as('true')
+    end
+    it "should not output its contents if the current page has a sibling next in order" do
+      page(:doc).should render('<r:siblings:unless_next>true</r:siblings:unless_next>').as('')
+    end
+  end
+  
+  describe "<r:siblings:if_previous>" do
+    it "should output its contents if the current page has a sibling previous in order" do
+      page(:doc).should render('<r:siblings:if_previous>true</r:siblings:if_previous>').as('true')
+    end
+    it "should not output its contents if the current page has no sibling previous in order" do
+      page(:sneezy).should render('<r:siblings:if_previous>true</r:siblings:if_previous>').as('')
+    end
+  end
+  
+  describe "<r:siblings:unless_previous>" do
+    it "should output its contents if the current page has no sibling previous in order" do
+      page(:sneezy).should render('<r:siblings:unless_previous>true</r:siblings:unless_previous>').as('true')
+    end
+    it "should not output its contents if the current page has a sibling previous in order" do
+      page(:doc).should render('<r:siblings:unless_previous>true</r:siblings:unless_previous>').as('')
+    end
+  end
+
   private
 
   def page(symbol = nil)

17c820229575cea716e1c789d3f8efcfc8855f83
spanner <will@spanner.org>
2011-06-29 09:31:33 +0100

    i18n, configuration, various updates and compatible with my version of dashboard as well as original

--- blog_extension.rb
+++ blog_extension.rb
@@ -6,10 +6,6 @@ class BlogExtension < Radiant::Extension
   description "Provides features for blogging."
   url "http://saturnflyer.com/"
   
-  # define_routes do |map|
-  #   map.connect 'admin/author/:action', :controller => 'admin/author'
-  # end
-  
   def activate
     if Page.table_exists? #allow bootstrapping
       Page.send :include, AuthorTags
@@ -18,13 +14,17 @@ class BlogExtension < Radiant::Extension
         has_many :pages, :foreign_key => :created_by_id unless self.respond_to?(:pages)
       }
       admin.user.edit.add :form, 'blog_details', :after => 'edit_notes'
+      admin.configuration.show.add :config, 'admin/configuration/blog', :after => 'defaults'
+      admin.configuration.edit.add :form,   'admin/configuration/edit_blog', :after => 'edit_defaults'
+
       if admin.respond_to?(:dashboard)
+        Admin::DashboardController.send :helper, Admin::BlogHelper
+
+        # some of these will not be in use, depending on which dashboard you've got, but it shouldn't matter.
+        admin.dashboard.index.add :shortcuts, 'new_blog_entry'
         admin.dashboard.index.add :current_user_draft_pages_top, 'new_page_link'
         admin.dashboard.index.add :current_user_published_pages_top, 'new_page_link'
         admin.dashboard.index.add :user_action_list, 'new_post_link'
-        Admin::DashboardController.class_eval{
-          helper Admin::BlogHelper
-        }
       end
     end
   end
--- config/initializers/radiant_config.rb
+++ config/initializers/radiant_config.rb
@@ -1,6 +1,9 @@
 Radiant.config do |config|
-  config.namespace 'blog' do
-    blog.define 'location.configurable?', :default => false
-    blog.define 'location.default', :default => ""
+  config.namespace('blog.location') do |blog|
+    blog.define 'configurable?', :default => false
+    blog.define 'default', :allow_blank => true, :validate_with => lambda { |setting| 
+      page = Page.find_by_path(setting.value)
+      setting.errors.add :value, :not_a_page if page.nil? || page.is_a?(FileNotFoundPage)
+    }
   end
-end 
+end
